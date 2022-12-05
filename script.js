427bd6b320f7c7a0728f4049b13e5d958fa11204
eszw10 <erycszw10@gmail.com>
2022-12-05 20:58:26 +0700

    fix grid script and add new function

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 let container = document.createElement('div')
-let grid = document.createElement('div')
-grid.classList.add('bord')
+let btn = document.createElement('input')
+document.body.appendChild(btn)
 document.body.appendChild(container)
 container.classList.add('container')
+btn.setAttribute('type','button')
+btn.setAttribute('value','Click Me!') 
+btn.classList.add('btn')
 
 function createRow(num) {
-    for(let i = 0;i < num;i++) { 
-        for(let j = 0;j < num;j++) {
-            let row = document.createElement('div')
-            row.style.height = `${960/num}px`
-            row.style.width = `${960/num}px`
-            container.appendChild(row)
-            row.classList.add('bord')
-            row.addEventListener('mouseover',()=> {
-                row.style.backgroundColor = 'black'
-            })
-        }     
+    if(num > 100) {
+        alert('Grid size should be less than 100!')
+    } else if(num <= 0) {
+        alert('Grid size should be more than zero!')
+    } else {
+        for(let i = 0;i < num;i++) { 
+            for(let j = 0;j < num;j++) {
+                let row = document.createElement('div')
+                row.style.height = `${960/num}px`
+                row.style.width = `${960/num}px`
+                container.appendChild(row)
+                row.classList.add('bord')
+                row.addEventListener('mouseover',()=> {
+                    row.style.backgroundColor = 'black'
+                })
+            }     
+        }
+        container.style.gridTemplateColumns = 'auto '.repeat(num)
     }
-    
 }
 
-createRow(10);
+btn.addEventListener('click', ()=> {
+    let num = Number(prompt('Input grid size : '))
+    createRow(num);
+})
+
